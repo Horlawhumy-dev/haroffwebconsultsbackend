a9f7c75cedf73b593c17e1d03385e9fe1ccd9ec6
Arafat Olayiwola <harof.dev@gmail.com>
2021-02-26 19:02:39 +0100

    linked nav links to the their templates

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,8 +3,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'homepage'
+app_name =  'homepage'
 
 urlpatterns = [
-    path('', views.index, name="homepage"),
+    path('', views.index, name="index"),
 ]
