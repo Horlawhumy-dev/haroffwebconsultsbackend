1f3a5e49660e42c5cce676340649b9c57dcbe795
Arafat Olayiwola <harof.dev@gmail.com>
2021-02-26 21:05:23 +0100

    modified settings for mail backend

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.core.mail  import send_mail
 
 # Create your views here.
 
 def contact(request):
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        fullname = request.POST['fullname']
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        # Send Mail
+        send_mail(
+            fullname, # Fullname of messenger
+            subject, # Subject or title of the message 
+            email, # email of the messenger, from
+            ['harof.dev@gmail.comm',] # To mail
+        )
+
+        context = {'fullname': fullname}
+    return render(request, 'contact/contact.html', context)
--- haroffwebconsults/settings.py
+++ haroffwebconsults/settings.py
@@ -123,3 +123,12 @@ STATIC_ROOT = 'haroffwebconsults/assets/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+
+
+# Email Settings
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '1025'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
